747916a39a04a31aa640b68c3528c99aa00b3c1d
Jakub Woszczek <icefreestyle2000@gmail.com>
2025-04-03 13:52:11 +0200

    zad 2.1 setup

---
+++ lab2/zad2/zadanie2.test.js
@@ -0,0 +1,10 @@
+function test_sum() {
+    describe('The sum() function', function () {
+        it('should return 4 for 2+2', function () {
+            sum(2, 2).should.equal(4);
+        });
+        it('should return 0 for -2+2', function () {
+            sum(-2, 2).should.equal(0);
+        });
+    });
+}

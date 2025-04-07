e3c1a3e370b3a9e84999104fba984d175ecee3a9
Jakub Woszczek <icefreestyle2000@gmail.com>
2025-04-07 18:37:50 +0200

    Zad2 done (mocha test sus)

--- lab2/zad2/zadanie2.js
+++ lab2/zad2/zadanie2.js
@@ -1,3 +1,49 @@
 export function sum(x,y) {
     return x+y;
 }
+
+export function sum_strings(a) {
+    var sum = 0;
+    console.log(a);
+    for (const  el of a) {
+        console.log(el);
+        if (!isNaN(parseFloat(el))) {
+
+            sum += parseFloat(el)
+        }
+    }
+    return sum;
+}
+
+export function digits(s) {
+    function is_even(num) {
+        return num % 2 === 0;
+    }
+
+    let orginal_num = Number(s)
+    let even_sum = 0;
+    let odd_sum = 0;
+    while (orginal_num > 0) {
+        const rest = orginal_num % 10;
+        if (is_even(rest)) {
+            even_sum += rest;
+        } else {
+            odd_sum += rest;
+        }
+        Math.floor(orginal_num / 10)
+    }
+    return[odd_sum,even_sum];
+}
+
+export function letters(s) {
+    let lower_case = 0;
+    let upper_case = 0;
+    for (const el of s) {
+        if (el === el.toUpperCase()) {
+            upper_case++;
+        } else {
+            lower_case++;
+        }
+    }
+    return [lower_case, upper_case];
+}
